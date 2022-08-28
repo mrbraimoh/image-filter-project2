dbcfca61c790e1e44b1d85774324be65c41d9809
PETER BRAIMOH <peterbraimoh98@gmail.com>
2022-08-28 15:28:39 +0100

    more changes

--- src/server.ts
+++ src/server.ts
@@ -32,7 +32,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   //! END @TODO1
 
-  app.get("/filteredimage/",async (req: Request,res: Response)=>{
+  app.get("/filteredimage/", async (req: Request,res: Response)=>{
     let {image_url}: any = req.query;
     if( image_url ) {
       filterImageFromURL(image_url).then((result)=>{
