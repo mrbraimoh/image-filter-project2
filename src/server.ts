1a886d0e0ed18fb658fdc1223b47dbe384605207
PETER BRAIMOH <peterbraimoh98@gmail.com>
2022-08-28 15:27:49 +0100

    changes to code

--- src/server.ts
+++ src/server.ts
@@ -32,20 +32,19 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   //! END @TODO1
 
-  app.get("/filteredimage",async (req: Request, res: Response) => {
-    const image_url = req.query.image_url.toString;
-
-    if (!image_url) {
-      res.status(422).send('Image Url is required')
+  app.get("/filteredimage/",async (req: Request,res: Response)=>{
+    let {image_url}: any = req.query;
+    if( image_url ) {
+      filterImageFromURL(image_url).then((result)=>{
+        res.sendFile(result);
+        res.on(`finish`,()=>deleteLocalFiles([result]));
+        }).catch((err)=>res.status(422).send(err))
     }
-
-    const imageFilter = await filterImageFromURL(image_url);
-
-    res.status(200).sendFile(imageFilter, () => {
-      deleteLocalFiles([imageFilter])
-    })
-  })
-  
+      else{
+        return res.status(422)
+                .send('This cannot be processed');
+      }
+  } );
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
